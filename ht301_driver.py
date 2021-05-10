69d11a54b50590bf118c9b18da22dbd857938ef6
MCMH2000 <MCMH2000@users.noreply.github.com>
2021-05-11 01:08:14 +0200

    Add driver

---
+++ ht301_driver.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+import math
+import numpy as np
+import cv2
+import math
+import ht301_hacklib
+import utils
+import time
+import os
+import sys
+import cv2
+import fcntl
+#import asyncio
+#from mavsdk import System
+from dronekit import connect
+from v4l2 import (
+    v4l2_format, VIDIOC_G_FMT, V4L2_BUF_TYPE_VIDEO_OUTPUT, V4L2_PIX_FMT_RGB24,
+    V4L2_FIELD_NONE, VIDIOC_S_FMT
+)
+
+########### init ###########
+##vehicle = connect("tcp://localhost:5761", wait_ready=False) #address of the openhd mavlink router
+
+rc_channel = '8' # aux channel 4 = channel 8
+ch_state = False
+prev_ch_state = False
+
+VIDEO_IN = "/dev/video0"
+VIDEO_OUT = "/dev/video5"
+VID_WIDTH = 384
+VID_HEIGHT = 288
+
+draw_temp = True
+
+colormaps = [cv2.COLORMAP_BONE, cv2.COLORMAP_PINK, cv2.COLORMAP_INFERNO, cv2.COLORMAP_JET] # list of used colormaps. all available colormaps here: https://docs.opencv.org/master/d3/d50/group__imgproc__colormap.html#ga9a805d8262bcbe273f16be9ea2055a65
+selectedmap = 1 # selected map on startup. default is 0
+
+############################
+
+# cycle colormaps
+def cyclecolormaps():
+    if selectedmap < len(colormaps) - 1:
+        selectedmap += 1
+    else:
+        selectedmap = 0
+
+# main
+def main():
+
+    ########### camera #############
+    cap = ht301_hacklib.HT301() # create camera object. initial calibration will be executed by contructor
+
+    videooutput = os.open(VIDEO_OUT, os.O_RDWR) # open v4l2 output device
+
+    # configure params for output device
+    vid_format = v4l2_format()
+    vid_format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT
+    if fcntl.ioctl(videooutput, VIDIOC_G_FMT, vid_format) < 0:
+        print("ERROR: unable to get video format!")
+        return -1
+
+    framesize = VID_WIDTH * VID_HEIGHT * 3
+    vid_format.fmt.pix.width = VID_WIDTH
+    vid_format.fmt.pix.height = VID_HEIGHT
+
+    # NOTE: change this according to below filters...
+    # Chose one from the supported formats on Chrome: YUV420, Y16, Z16, INVZ,
+    # YUYV, RGB24, MJPEG, JPEG
+    vid_format.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24
+    vid_format.fmt.pix.sizeimage = framesize
+    vid_format.fmt.pix.field = V4L2_FIELD_NONE
+
+    if fcntl.ioctl(videooutput, VIDIOC_S_FMT, vid_format) < 0:
+        print("ERROR: unable to set video format!")
+        return -1
+
+    ################################
+
+    while(True):
+        # cycle only one map per button press
+        ##if vehicle.channels[rc_channel] > 1800:
+        ##    ch_state = True
+        ##    if prev_ch_state == False:
+        ##        cyclecolormaps()
+        ##else:
+        ##    ch_state = False
+        ##prev_ch_state = ch_state
+
+
+        ret, frame = cap.read() # read frame from thermal camera
+        info, lut = cap.info() # get hottest and coldest spot and its temperatures
+        frame = frame.astype(np.float32)
+
+        if selectedmap != 0: # do not apply anything when 0 is selected. original frame
+            # Sketchy auto-exposure
+            frame -= frame.min()
+            frame /= frame.max()
+            frame = (np.clip(frame, 0, 1)*255).astype(np.uint8)
+            frame = cv2.applyColorMap(frame, colormaps[selectedmap]) # apply colormap
+
+        # rotate the temperature points with the image
+        (coldx, coldy) = info['Tmin_point']
+        (warmx, warmy) = info['Tmax_point']
+        coldestpoint = (VID_WIDTH - coldx, VID_HEIGHT - coldy)
+        warmestpoint = (VID_WIDTH - warmx, VID_HEIGHT - warmy)
+
+        frame = cv2.flip(frame, -1)
+
+        if draw_temp:
+            utils.drawTemperature(frame, coldestpoint, info['Tmin_C'], (0,0,255)) # coldest spot
+            utils.drawTemperature(frame, warmestpoint, info['Tmax_C'], (255,0,0)) # hottest spot
+            #utils.drawTemperature(frame, info['Tcenter_point'], info['Tcenter_C'], (0,255,255)) # center spot
+
+        # write frame to output device
+        written = os.write(videooutput, frame.data)
+
+    cap.release()
+    ##vehicle.close()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
