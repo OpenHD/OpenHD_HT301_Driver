6a33f3bbc8aaf46facb9e1b67c407406a96162a6
MCMH2000 <MCMH2000@users.noreply.github.com>
2021-05-14 15:53:21 +0200

    bugfix: inverted colors
    OpenCV BGR frame was written in the v4l2 RGB device and therefore generated weird colormaps

--- ht301_driver.py
+++ ht301_driver.py
@@ -37,10 +37,7 @@ calibration_offset = True
 
 # list of used colormaps. all available colormaps here: https://docs.opencv.org/master/d3/d50/group__imgproc__colormap.html#ga9a805d8262bcbe273f16be9ea2055a65
 colormaps = [cv2.COLORMAP_BONE, cv2.COLORMAP_PINK, -1, cv2.COLORMAP_INFERNO, cv2.COLORMAP_TURBO] # add -1 for dde algorithm
-selectedmap = 2 # selected map on startup. default is 0
-
-# create a Contrast Limited Adaptive Histogram Equalization object.
-clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(10,10))
+selectedmap = 1 # selected map on startup. default is 0
 
 ############################
 
@@ -58,10 +55,16 @@ def main():
     # create camera object. initial calibration will be executed by constructor
     cap = ht301_hacklib.HT301()
 
+    # read calibration array from file
+    offset = np.load("noise_pattern_calibration.npy")
+
+    # create a Contrast Limited Adaptive Histogram Equalization object.
+    clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+
     # open v4l2 output device
     videooutput = os.open(VIDEO_OUT, os.O_RDWR)
 
-    # configure params for output device
+    # configure parameters for output device
     vid_format = v4l2_format()
     vid_format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT
     if fcntl.ioctl(videooutput, VIDIOC_G_FMT, vid_format) < 0:
@@ -83,9 +86,7 @@ def main():
 
     ################################
 
-    # read calibration array from file
-    offset = np.load("noise_pattern_calibration.npy")
-
+    # video loop
     while(True):
         # cycle only one map per button press
         #if vehicle.channels[rc_channel] > 1800:
@@ -96,29 +97,24 @@ def main():
         #    ch_state = False
         #prev_ch_state = ch_state
 
-
         ret, frame = cap.read() # read frame from thermal camera
-        orig_frame = frame
         info, lut = cap.info() # get hottest and coldest spot and its temperatures
 
         if selectedmap != 0: # do not apply anything when 0 is selected. original frame
-            # sketchy auto-exposure
+            # automatic gain control
             frame = frame.astype(np.float32)
-            if calibration_offset == True:
+            if calibration_offset == True: # apply calibration offset
                 frame = frame - offset + np.mean(offset)
-            frame = 255*(frame - frame.min())/(frame.max()-frame.min()) # todo: better detail consideration wothout clipping extreme values see next line
-            frame = np.clip(frame, 0, 255).astype(np.uint8)
+            frame = (255*((frame - frame.min())/(frame.max()-frame.min()))).astype(np.uint8) # cast frame to values from 0 to 255
+            #frame = np.clip(frame, 0, 255).astype(np.uint8)
 
             if colormaps[selectedmap] == -1:
-                # dde algorithm implementation
-                framebuffer = frame #buffer
+                # digital detail enhancement algorithm
+                framebuffer = frame
                 clahe.apply(framebuffer, frame)
-                #frame = (255-frame) # invert range if colormap is inverted
                 frame = cv2.applyColorMap(frame, colormaps[1])
-
             else:
                 # apply colormap
-                #frame = (255-frame) # invert range if colormap is inverted
                 frame = cv2.applyColorMap(frame, colormaps[selectedmap])
 
         if flipped_camera == True: # rotate the temperature points with the image if the thermal camera is mounted upside down
@@ -136,8 +132,9 @@ def main():
             utils.drawTemperature(frame, warmestpoint, info['Tmax_C'], (255,0,0)) # hottest spot
             #utils.drawTemperature(frame, info['Tcenter_point'], info['Tcenter_C'], (255,255,0)) # center spot
 
-        # write frame to output device
-        written = os.write(videooutput, frame.data)
+        # output
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert opencv bgr to rgb for the v4l2 pixelformat
+        written = os.write(videooutput, frame.data) # write frame to output device
 
     cap.release()
     #vehicle.close()
